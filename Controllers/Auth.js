e2bb75e2fd5969d44a48f0fdbafac67cc0411270
Amar <amar2115425@akgec.ac.in>
2024-07-21 03:37:35 +0530

    login added

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -1,5 +1,7 @@
 const bcrypt  = require("bcrypt");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 //signup route handler
 
@@ -47,6 +49,81 @@ exports.signup = async(req,res)=>{
         .json({
             success:false,
             message:"Error while signup",
+        })
+    }
+}
+
+//login 
+exports.login = async(req,res)=>{
+    try{
+        //data fetch
+        const {email,password} = req.body;
+        
+        //validation on email & password
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message:"Please fill all the details successfully",
+            });
+        }
+
+        //check for registered user
+        const user = await User.findOne({email});
+        //if not a regitered user
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message:"User is not registered",
+            });
+        }
+
+        const payload = {
+            email : user.email,
+            id : user._id,
+            role:user.role,
+        }
+
+        //verify password and generate a JWT taken
+        if(await bcrypt.compare(password,user.password)){
+            //password matched
+            let token = jwt.sign(payload, 
+                process.env.JWT_SECRET,
+                {
+                    expiresIn:"2h",
+                }
+            );
+            // Removed unnecessary user.token = token assignment: The token is already sent in the response, no need to attach it to the user object.
+            // user.token = token;
+            user.password = undefined;
+
+            const options = {
+                expires:new Date(Date.now() + 3*24*60*60*1000),
+                httpOnly:true,
+            }
+
+            res.cookie("token",token, options)
+            .status(200).json({
+                success:true,
+                token,
+                user,
+                message:"User Logged Successfully",
+            });
+        }
+        else{
+            //password does not found
+            return res.status(403).json({
+                success:false,
+                message:"Password Incorrect",
+            });
+        }
+    }
+    catch(err){
+        console.error(err);
+        console.log(err);
+        res.status(500)
+        .json({
+            success:false,
+            message:"Error while logging in",
         })
     }
 }
