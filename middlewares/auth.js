c0f57e92976089757020b0f8f6052be2c427551b
Amar <amar2115425@akgec.ac.in>
2024-07-25 22:14:25 +0530

    authorization completed

---
+++ middlewares/auth.js
@@ -0,0 +1,67 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+exports.auth = (req, res, next)=>{
+    try{
+        const token = req.body.token;
+
+        if(!token){
+            return res.status(401).json({
+                success:false,
+                meassage:"Token Missing"
+            })
+        }
+
+        //verify the token
+        try{
+            const decode = jwt.verify(token,process.env.JWT_SECRET);
+            req.user = decode;
+            next();
+        }catch(err){
+            return res.status(401).json({
+                success:false,
+                meassage:"Token is Invalid"
+            });
+        }
+    }catch(err){
+        return res.status(401).json({
+            success:false,
+            meassage:"Something went wrong while verifying the token"
+        });
+    }
+};
+
+
+exports.isStudent = (req, res, next) => {
+    try{
+        if(req.user.role!=="Student"){
+            return res.status(401).json({
+                success:false,
+                message:"This is a Protected Route for students"
+            });
+        }
+        next();
+    }catch(err){
+        return res.status(500).json({
+            success:false,
+            message:"user role is not matching"
+        });
+    }
+}
+
+exports.isAdmin = (req, res, next) => {
+    try{
+        if(req.user.role!=="Admin"){
+            return res.status(401).json({
+                success:false,
+                message:"This is a Protected Route for Admin"
+            });
+        }
+        next();
+    }catch(err){
+        return res.status(500).json({
+            success:false,
+            message:"user role is not matching"
+        });
+    }
+}
--- routes/User.js
+++ routes/User.js
@@ -2,8 +2,34 @@ const express = require("express");
 const router = express.Router();
 
 const { signup, login} = require("../Controllers/Auth");
+const {auth, isStudent, isAdmin }=require("../middlewares/auth");
 
- router.post("/login",login);
+
+router.post("/login",login);
 router.post("/signup",signup);
+
+//protected route
+
+//test route 
+router.get("/test", auth,  (req,res)=>{
+    res.json({
+        success:true,
+        message:'Welcome to Protected Route for test',
+    })
+});
+
+router.get("/student", auth, isStudent, (req,res)=>{
+    res.json({
+        success:true,
+        message:'Welcome to Protected Route for Students',
+    })
+});
+
+router.get("/admin", auth, isAdmin, (req,res)=>{
+    res.json({
+        success:true,
+        message:'Welcome to Protected Route for Admin',
+    })
+});
 
 module.exports = router;
